428f577403bd5cf5936f71728862840468c45ca0
Royals Central <danny.zamora@gmail.com>
2018-08-18 19:30:35 -0500

    push updates

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Gross Burger', false);
